531ccc0323276d31bf3b735579cc6f67b723e899
PythonMyLife <1425439522@qq.com>
2020-05-07 22:05:58 +0800

    deal with bugs in post

--- e-map-spring/src/main/java/backend/Controller/UserController.java
+++ e-map-spring/src/main/java/backend/Controller/UserController.java
@@ -3,10 +3,9 @@ package backend.Controller;
 import backend.Service.UserService;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -16,25 +15,34 @@ public class UserController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject register(String name, String phone, String password) {
+    public JSONObject register(@RequestBody Map<String, String> map) {
+        String name = map.get("name");
+        String phone = map.get("phone");
+        String password = map.get("password");
         return userService.register(name, phone, password);
     }
 
     @RequestMapping(value = "/admin/register", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject registerAdmin(String name, String password) {
+    public JSONObject registerAdmin(@RequestBody Map<String, String> map) {
+        String name = map.get("name");
+        String password = map.get("password");
         return userService.registerAdmin(name, password);
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject login(String name, String password) {
+    public JSONObject login(@RequestBody Map<String, String> map) {
+        String name = map.get("name");
+        String password = map.get("password");
         return userService.login(name, password);
     }
 
     @RequestMapping(value = "/admin/login", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject loginAdmin(String name, String password) {
+    public JSONObject loginAdmin(@RequestBody Map<String, String> map) {
+        String name = map.get("name");
+        String password = map.get("password");
         return userService.loginAdmin(name, password);
     }
 }
